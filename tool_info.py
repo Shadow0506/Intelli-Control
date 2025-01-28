5d0ffbc1289e0fea48f8d43a598c54ef9ce4a648
Shadow <bhavyapratap0506@gmail.com>
2025-01-28 19:19:26 +0530

    changed format to facilitate retrieval

--- tool_info.py
+++ tool_info.py
@@ -1,92 +1,95 @@
-tool_list = [
-    {
-      "name": "create_directory",
-      "description": "Creates a new directory with the specified name.",
-      "parameters": {
-        "folder_name": "str - The name of the folder to create.",
-        "folder_creating_path": "str - The path where the new folder is to be created"
-      },
-      "required": ["folder_name", "folder_path"],
-      "defaults": {
-        "folder_name": "NoFolderNameFound",
-        "folder_path": "E:\\"
-      },
-      "keywords": ["create directory", "make folder", "new directory", "filesystem"]
+tool_list = {
+    "open_app": {
+        "tool_name": "open_app",
+        "description": "Opens the specified application by name.",
+        "parameters": {
+            "app_name": "str - The name of the application to open. Do NOT include path or extension."
+        }
     },
-    {
-      "name": "delete_directory",
-      "description": "Deletes the specified directory if it exists.",
-      "parameters": {
-        "folder_name": "str - The name of the folder to remove."
-      },
-      "required": ["folder_name"],
-      "keywords": ["delete directory", "remove folder", "filesystem", "delete"]
+    "close_app": {
+        "tool_name": "close_app",
+        "description": "Closes the specified application by name.",
+        "parameters": {
+            "app_name": "str - The name of the application to close. Do NOT include path or extension."
+        }
     },
-    {
-      "name": "open_directory",
-      "description": "Opens the specified directory path in the file explorer.",
-      "parameters": {
-        "folder_path": "str - The path of the folder to open."
-      },
-      "required": ["folder_name"],
-      "keywords": ["open directory", "open folder", "file explorer", "navigate"]
+    "open_url": {
+        "tool_name": "open_url",
+        "description": "Opens the specified URL in the default web browser.",
+        "parameters": {
+            "url": "str - The URL to open."
+        }
     },
-    {
-      "name": "open_file",
-      "description": "Opens the specified file with its default associated application.",
-      "parameters": {
-        "file_name": "str - The name or path of the file to open."
-      },
-      "required": ["file_name"],
-      "keywords": ["open file", "file explorer", "launch file", "default application"]
+    "create_directory": {
+        "tool_name": "create_directory",
+        "description": "Creates a new directory with the specified name.",
+        "parameters": {
+            "folder_name": "str - The name of the folder to create.",
+            "folder_creation_path": "str - The ABSOLUTE path where the new folder is to be created"
+        }
     },
-    {
-      "name": "create_file",
-      "description": "Creates a new empty file with the specified name.",
-      "parameters": {
-        "file_name": "str - The name or path of the file to create."
-      },
-      "required": ["file_name"],
-      "keywords": ["create file", "new file", "file system", "generate file"]
+    "delete_directory": {
+        "tool_name": "delete_directory",
+        "description": "Deletes the specified directory if it exists.",
+        "parameters": {
+            "folder_PATH": "str - The ABSOLUTE path of the folder to remove."
+        }
     },
-    {
-      "name": "delete_file",
-      "description": "Deletes the specified file if it exists.",
-      "parameters": {
-        "file_name": "str - The name or path of the file to delete."
-      },
-      "required": ["file_name"],
-      "keywords": ["delete file", "remove file", "filesystem", "delete"]
+    "open_directory": {
+        "tool_name": "open_directory",
+        "description": "Opens the specified directory path in the file explorer.",
+        "parameters": {
+            "folder_path": "str - The ABSOLUTE path of the folder to open."
+        }
     },
-    {
-      "name": "save_file",
-      "description": "Saves the current file in use, typically for an application or editor.",
-      "parameters": {},
-      "required": [],
-      "keywords": ["save file", "store file", "save changes", "editor"]
+    "open_file": {
+        "tool_name": "open_file",
+        "description": "Opens the specified file with its default associated application.",
+        "parameters": {
+            "file_path": "str - The ABSOLUTE path of the file to open."
+        }
     },
-    {
-      "name": "move_mouse_to_coordinates",
-      "description": "Moves the mouse cursor to the specified screen coordinates.",
-      "parameters": {
-        "x": "float - The x-coordinate to move the mouse to.",
-        "y": "float - The y-coordinate to move the mouse to."
-      },
-      "required": [],
-      "defaults": {
-        "x": 0,
-        "y": 0
-      },
-      "keywords": ["mouse movement", "cursor position", "move mouse", "coordinates"]
+    "create_file": {
+        "tool_name": "create_file",
+        "description": "Creates a new empty file with the specified name.",
+        "parameters": {
+            "file_name": "str - The name of the file to create.",
+            "file_path": "str - The ABSOLUTE path of the file to create."
+        }
     },
-    {
-      "name": "two_key_shortcuts",
-      "description": "Simulates pressing two keys simultaneously as a shortcut.",
-      "parameters": {
-        "key1": "str - The first key in the shortcut.",
-        "key2": "str - The second key in the shortcut."
-      },
-      "required": ["key1", "key2"],
-      "keywords": ["keyboard shortcut", "key combination", "hotkeys", "simulate keys"]
+    "delete_file": {
+        "tool_name": "delete_file",
+        "description": "Deletes the specified file if it exists.",
+        "parameters": {
+            "file_path": "str - The ABSOLUTE path of the file to delete."
+        }
+    },
+    "save_file": {
+        "tool_name": "save_file",
+        "description": "Saves the current file in use, typically for an application or editor.",
+        "parameters": {}
+    },
+    "move_mouse_to_coordinates": {
+        "tool_name": "move_mouse_to_coordinates",
+        "description": "Moves the mouse cursor to the specified screen coordinates.",
+        "parameters": {
+            "x": "float - The x-coordinate to move the mouse to.",
+            "y": "float - The y-coordinate to move the mouse to."
+        }
+    },
+    "two_key_shortcuts": {
+        "tool_name": "two_key_shortcuts",
+        "description": "Simulates pressing two keys simultaneously as a shortcut.",
+        "parameters": {
+            "key1": "str - The first key in the shortcut.",
+            "key2": "str - The second key in the shortcut."
+        }
+    },
+    "web_scrape": {
+        "tool_name": "web_scrape",
+        "description": "Takes the search query and returns the top 3 URLs from the web.",
+        "parameters": {
+            "query": "str - The search query to scrape the web for."
+        }
     }
-]  
+}
