e981e480d9d021bca38bd1c9044f3c03c45aa0db
Shadow <bhavyapratap0506@gmail.com>
2025-08-13 13:37:33 +0530

    removed extra file

--- prompts.py
+++
@@ -1,6 +0,0 @@
-RETRIEVE_TOOLS = """
-Here is the list of tools available to you, select the required tools for the user query and generate a chain of thought.
-User query: {user_query}
-Tools:
-1. 
-"""
